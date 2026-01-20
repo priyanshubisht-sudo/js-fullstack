76a6695da97cd2dde02936ed753070876b55ab83
priyanshubisht-sudo <thepriyanshubisht@gmail.com>
2026-01-20 20:25:59 +0530

    about datatypes in JS.

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as the newer version.
+
+// alert(3+3) we are using node, not browser
+
+console.log(3+3)
+console.log("Hello World")
+
+let name = "Priyanshu"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value *not 0*
+// undefined => not defined at all
+// symbol => for uniqueness
+
+// objects
+
+console.log(typeof null); // output - object!!
+console.log(typeof isLoggedIn); 
+console.log(typeof undefined); // output - undefined
