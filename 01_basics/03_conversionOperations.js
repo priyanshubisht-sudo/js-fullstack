3f2faac57ae55fbd9cc8777cc893de16638760bc
priyanshubisht-sudo <thepriyanshubisht@gmail.com>
2026-01-21 20:53:22 +0530

    about datatype conversions in JS.

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -15,12 +15,44 @@ let valueInNumber = Number(score)
 let isLoggedIn = "priyanshu"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true, 0 => false
 // "" => false, "priyanshu" => true
 
 let someNumber = 33;
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ****************************** Operations ********************************** //
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Priyanshu"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(true)
+// console.log(+true)
+// console.log(false)
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+gameCounter++;
+
+console.log(gameCounter)
