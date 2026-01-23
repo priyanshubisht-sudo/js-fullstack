07a14d2f4e978b84f066a1c98cdad0b52159380c
priyanshubisht-sudo <thepriyanshubisht@gmail.com>
2026-01-23 13:13:11 +0530

    about comparision in JS.

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+console.log("2" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null <= 0);
+
+// different methods used to compare in comparision
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined <= 0);
+
+// strict check -> "==="
+
+console.log("2" === 1);
