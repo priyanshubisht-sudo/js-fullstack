aee0897ef8b7ee244f14eeb926da18cf00ca65a1
priyanshubisht-sudo <thepriyanshubisht@gmail.com>
2026-02-15 23:45:02 +0530

    about for loops and break, continue in JS.

---
+++ 05_iterations.js/one.js
@@ -0,0 +1,40 @@
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    // console.log(element);    
+}
+
+for (let i = 1; i <= 10; i++) {
+    //console.log(`Outer loop value: ${i}`);
+   for (let j = 1; j <= 10; j++) {
+    //console.log(`Inner loop value ${j} and inner loop ${i}`);
+    //console.log(i + '*' + j + ' = ' + i*j );
+   }
+    
+}
+
+let myArray = ["Flash", "Superman", "Batman"]
+console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+
+//break
+
+for (let i = 1; i <=20 ; i++) {
+    const element = i;
+    if (i == 5) {
+        console.log(`Detected  5`);
+        break
+    }
+    // console.log(element);
+}
+for (let i = 1; i <=20 ; i++) {
+    const element = i;
+    if (i == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(element);
+}
